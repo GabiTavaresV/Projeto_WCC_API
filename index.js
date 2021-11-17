c65e0eff23f9bc0e5008c66fc5d30e5cf5eceb1f
gabriela.ttavares <gabriela.ttavares@gmail.com>
2021-11-16 21:53:05 -0300

    Base de sistema de artigos

--- index.js
+++ index.js
@@ -3,33 +3,8 @@ const app = express();
 const port = 8080;
 
 app.get("/", function(req, res){
-    res.send("Minha primeira requisição!")
+    res.send("Dasa Educa - Artigos")
 });
-
-app.get("/segunda-req", function(req, res){
-    res.send("minha SEGUNDA requisição!")
-});
-
-
-app.get("/com-parametros", function(req, res){
-    res.send("Com parametros funciona!, Sabadou "  +  req.query.nome)
-});
-
-app.post("/meu-orimeiro-post", function(req, res){
-    console.log(req.body);
-    res.send("meu post funciona");
-});
-
-app.put("/meu-primeiro-put/:id", function(req, res){
-    console.log(req.body, req.params.id);
-    res.send("meu put funciona");
-});
-
-app.delete("/meu-primeiro-delete/:id", function(req, res){
-    console.log(re.params.id)
-    res.send("Meu delete funciona" + req.params.id)
-})
-
 
 
 app.listen(port, function(){
